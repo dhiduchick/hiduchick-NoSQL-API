201e2fea68e64f2d33d7b53f3de26f31073a0787
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-19 20:21:08 -0500

    add friend function

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,4 +55,22 @@ module.exports = {
             res.status(500).json(err)
         }
     },
+    async addFriend(req,res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                {$addToSet: {friends: req.params.friendId}},
+                {runValidators: true, new:true}
+            )
+            if(!user) {
+                return res.status(400).json({message:'No user found with that ID'};)
+            }
+            res.json(user);
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        },
+
+        
+    }
 }
